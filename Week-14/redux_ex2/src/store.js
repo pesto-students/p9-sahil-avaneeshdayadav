c3e39ee83c8f56b8bbc766edb8077dd555dd2448
Avaneesh Yadav <60056707+avaneeshdayadav@users.noreply.github.com>
2023-05-22 23:07:50 +0530

    Create store.js

---
+++ Week-14/redux_ex2/src/store.js
@@ -0,0 +1,19 @@
+import { createStore } from "redux";
+
+const initialState = {
+  steps: 0
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_STEP":
+      return { steps: state.steps + 1 };
+    case "RESET":
+      return { steps: 0 };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+export default store;
